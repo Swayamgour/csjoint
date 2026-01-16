5d03dc57931379ec0cd9565dc5f017784c56c458
Swayam Gaur <goura0775@gmail.com>
2026-01-16 15:47:19 +0530

    kjh

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -11,9 +11,7 @@ function Courses() {
     }
     return (
         <>
-            {/* @extends('layout.main')
-            @section('content')
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> */}
+
 
 
             <CustomHeader heading={data.heading} text={data.text} />
@@ -90,13 +88,13 @@ function Courses() {
                         </div>
                         <div className="tab-pane fade " id="c2">
                             <div className="course-tab-card">
-                                <h3 className="course-tab-card-title">Tab-2 Content :-10 days Services Selection Board Hackathon</h3>
-                                <p>INR 11000/- · 18% GST</p>
-                                <h3 className="course-tab-card-hours"><strong>Total Sessions:</strong> 10 | <strong>Total Learning Hours:</strong> 40</h3>
+                                <h3 className="course-tab-card-title">Tab-2 Content :- Introduction to SSB & PPDT</h3>
+                                {/* <p>INR 11000/- · 18% GST</p> */}
+                                {/* <h3 className="course-tab-card-hours"><strong>Total Sessions:</strong> 10 | <strong>Total Learning Hours:</strong> 40</h3> */}
 
                                 <h5 className="mb-2">Topic Covered:</h5>
                                 <ul>
-                                    <li>Introduction to SSB, Complete SSB Procedure, Stage 1 Testing – OIR Test, Picture Perception & Discussion Test</li>
+                                    {/* <li>Introduction to SSB, Complete SSB Procedure, Stage 1 Testing – OIR Test, Picture Perception & Discussion Test</li>
                                     <li>PIQ Form and Interview Procedure</li>
                                     <li>Mock Interview by a DIPR certified Interviewing Officer</li>
                                     <li>Projective Technique Theory – Decoding the Psych Tests (Thematic Apperception Test, Word Association Test, Situation Reaction Test, Self-Description Test)</li>
@@ -106,43 +104,48 @@ function Courses() {
                                     <li>Officer Like Qualities Theory and the OLQ Correlation</li>
                                     <li>Conference Procedure</li>
                                     <li>Correlation amongst all three techniques of assessment (GTO, Psych, IO)</li>
-                                    <li>Doubt Clearing, SOCIOMETRY, Individual Feedback</li>
+                                    <li>Doubt Clearing, SOCIOMETRY, Individual Feedback</li> */}
+
+                                    <li>
+                                        Introduction to SSB, Complete SSB Procedure, Stage 1 Testing – OIR Test, Picture Perception & Discussion Test
+                                    </li>
                                 </ul>
 
                             </div>
                         </div>
                         <div className="tab-pane fade" id="c3">
                             <div className="course-tab-card">
-                                <h3 className="course-tab-card-title">Tab-3 Content :- 10 days Services Selection Board Hackathon</h3>
-                                <p>INR 11000/- · 18% GST</p>
-                                <h3 className="course-tab-card-hours"><strong>Total Sessions:</strong> 10 | <strong>Total Learning Hours:</strong> 40</h3>
+                                <h3 className="course-tab-card-title">Tab-3 Content :- Psych Theory Course</h3>
+                                {/* <p>INR 11000/- · 18% GST</p>
+                                <h3 className="course-tab-card-hours"><strong>Total Sessions:</strong> 10 | <strong>Total Learning Hours:</strong> 40</h3> */}
 
                                 <h5 className="mb-2">Topic Covered:</h5>
                                 <ul>
-                                    <li>Introduction to SSB, Complete SSB Procedure, Stage 1 Testing – OIR Test, Picture Perception & Discussion Test</li>
-                                    <li>PIQ Form and Interview Procedure</li>
-                                    <li>Mock Interview by a DIPR certified Interviewing Officer</li>
-                                    <li>Projective Technique Theory – Decoding the Psych Tests (Thematic Apperception Test, Word Association Test, Situation Reaction Test, Self-Description Test)</li>
-                                    <li>Mock Psych Test and feedback by a DIPR certified Psychologist</li>
-                                    <li>Theory and Concepts of the Group Situational Tasks – Group Discussion, Group Planning Exercise, Progressive Group Task, Group Obstacle Race, Half Group Task, Lecturette, Individual Obstacles, Command Task, Final Group Task</li>
-                                    <li>Genesis of the Group Testing Technique and what GTO looks at during the Group Testing</li>
-                                    <li>Officer Like Qualities Theory and the OLQ Correlation</li>
-                                    <li>Conference Procedure</li>
-                                    <li>Correlation amongst all three techniques of assessment (GTO, Psych, IO)</li>
-                                    <li>Doubt Clearing, SOCIOMETRY, Individual Feedback</li>
+                                    <li>
+                                        Projective Technique Theory – Decoding the Psych Tests
+                                    </li>
+
+
+                                    <li>Thematic Apperception Test</li>
+                                    <li> Word Association Test</li>
+                                    <li> Situation Reaction Test</li>
+                                    <li>Self-Description Test</li>
+                                    <li>
+                                        Mock Psych Test and feedback by a DIPR certified Psychologist.
+                                    </li>
                                 </ul>
 
                             </div>
                         </div>
                         <div className="tab-pane fade " id="c4">
                             <div className="course-tab-card">
-                                <h3 className="course-tab-card-title">Tab-4 Content :- 10 days Services Selection Board Hackathon</h3>
-                                <p>INR 11000/- · 18% GST</p>
-                                <h3 className="course-tab-card-hours"><strong>Total Sessions:</strong> 10 | <strong>Total Learning Hours:</strong> 40</h3>
+                                <h3 className="course-tab-card-title">Tab-4 Content :-  Interview Theory Course</h3>
+                                {/* <p>INR 11000/- · 18% GST</p>
+                                <h3 className="course-tab-card-hours"><strong>Total Sessions:</strong> 10 | <strong>Total Learning Hours:</strong> 40</h3> */}
 
                                 <h5 className="mb-2">Topic Covered:</h5>
                                 <ul>
-                                    <li>Introduction to SSB, Complete SSB Procedure, Stage 1 Testing – OIR Test, Picture Perception & Discussion Test</li>
+                                    {/* <li>Introduction to SSB, Complete SSB Procedure, Stage 1 Testing – OIR Test, Picture Perception & Discussion Test</li>
                                     <li>PIQ Form and Interview Procedure</li>
                                     <li>Mock Interview by a DIPR certified Interviewing Officer</li>
                                     <li>Projective Technique Theory – Decoding the Psych Tests (Thematic Apperception Test, Word Association Test, Situation Reaction Test, Self-Description Test)</li>
@@ -152,7 +155,14 @@ function Courses() {
                                     <li>Officer Like Qualities Theory and the OLQ Correlation</li>
                                     <li>Conference Procedure</li>
                                     <li>Correlation amongst all three techniques of assessment (GTO, Psych, IO)</li>
-                                    <li>Doubt Clearing, SOCIOMETRY, Individual Feedback</li>
+                                    <li>Doubt Clearing, SOCIOMETRY, Individual Feedback</li> */}
+                                    <li>
+                                        PIQ Form and Interview Procedure
+                                    </li>
+                                    <li>
+                                        Mock Interview and feedback by a DIPR certified Interviewing Officer.
+
+                                    </li>
                                 </ul>
 
                             </div>
@@ -160,24 +170,32 @@ function Courses() {
 
                         <div className="tab-pane fade" id="c5">
                             <div className="course-tab-card">
-                                <h3 className="course-tab-card-title"> Tab-5 Content :-10 days Services Selection Board Hackathon</h3>
-                                <p>INR 11000/- · 18% GST</p>
-                                <h3 className="course-tab-card-hours"><strong>Total Sessions:</strong> 10 | <strong>Total Learning Hours:</strong> 40</h3>
+                                <h3 className="course-tab-card-title"> Tab-5 Content :- Group Testing Course</h3>
+                                {/* <p>INR 11000/- · 18% GST</p>
+                                <h3 className="course-tab-card-hours"><strong>Total Sessions:</strong> 10 | <strong>Total Learning Hours:</strong> 40</h3> */}
 
                                 <h5 className="mb-2">Topic Covered:</h5>
+                                {/* <ul> */}
                                 <ul>
-                                    <li>Introduction to SSB, Complete SSB Procedure, Stage 1 Testing – OIR Test, Picture Perception & Discussion Test</li>
-                                    <li>PIQ Form and Interview Procedure</li>
-                                    <li>Mock Interview by a DIPR certified Interviewing Officer</li>
-                                    <li>Projective Technique Theory – Decoding the Psych Tests (Thematic Apperception Test, Word Association Test, Situation Reaction Test, Self-Description Test)</li>
-                                    <li>Mock Psych Test and feedback by a DIPR certified Psychologist</li>
-                                    <li>Theory and Concepts of the Group Situational Tasks – Group Discussion, Group Planning Exercise, Progressive Group Task, Group Obstacle Race, Half Group Task, Lecturette, Individual Obstacles, Command Task, Final Group Task</li>
-                                    <li>Genesis of the Group Testing Technique and what GTO looks at during the Group Testing</li>
-                                    <li>Officer Like Qualities Theory and the OLQ Correlation</li>
-                                    <li>Conference Procedure</li>
-                                    <li>Correlation amongst all three techniques of assessment (GTO, Psych, IO)</li>
-                                    <li>Doubt Clearing, SOCIOMETRY, Individual Feedback</li>
+                                    <li>Theory and Concepts of the Group Situational Tasks</li>
+                                    <li>Group Discussion</li>
+                                    <li>Group Planning Exercise</li>
+                                    <li>Progressive Group Task</li>
+                                    <li>Group Obstacle Race</li>
+                                    <li>Half Group Task</li>
+                                    <li>Lecturette</li>
+                                    <li>Individual Obstacles</li>
+                                    <li>Command Task</li>
+                                    <li>Final Group Task</li>
+                                    <li>
+                                        Genesis of the Group Testing Technique and what GTO looks at during the
+                                        Group Testing
+                                    </li>
+                                    <li>Feedback by a DIPR certified Group Testing Officer</li>
+                                    <li>The entire course is covered through a Digital GTO Ground</li>
                                 </ul>
+
+                                {/* </ul> */}
 
                             </div>
                         </div>
@@ -193,15 +211,5 @@ function Courses() {
     )
 }
 
-{/* <script>
-    document.getElementById('courseTabSelect').addEventListener('change', function() {
-        const target = this.value;
-        const tab = document.querySelector(
-            `.course-nav-tabs .nav-link[data-bs-target="${target}"]`
-        );
-        if (tab) {
-            new bootstrap.Tab(tab).show();
-        }
-    });
-    </script> */}
+
 export default Courses
