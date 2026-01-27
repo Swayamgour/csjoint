4183e41ad2dec8375bf6c5071b24d8b31bf8502f
Swayam Gaur <goura0775@gmail.com>
2026-01-27 20:06:15 +0530

    add profile fetching logic in LoadingScreen component

--- src/pages/LoadingScreen.jsx
+++ src/pages/LoadingScreen.jsx
@@ -1,8 +1,59 @@
+import { useEffect } from "react";
 import "../style/LoadingScreen.css";
+import axios from "axios";
 // import logo from "../assets/logo.png"; // 👉 apna logo yahan daalo
 
 export default function LoadingScreen() {
+
+    // useEffect(()
+
+
+    useEffect(() => {
+        const fetchProfile = async () => {
+            try {
+                const token = localStorage.getItem("authToken");
+
+                if (!token) {
+                    // token hi nahi hai
+                    localStorage.removeItem("authToken");
+                    // window.location.href = "/login";
+                    return;
+                }
+
+                const res = await axios.get(
+                    "https://api.ssbwithisv.in/api/user/profile",
+                    {
+                        headers: {
+                            Authorization: `Bearer ${token}`,
+                        },
+                    }
+                );
+
+                // agar backend ne bola token invalid
+                if (res.data.status !== "ok") {
+                    localStorage.removeItem("authToken");
+                    // window.location.href = "/login";
+                }
+
+                console.log("User authenticated", res.data);
+
+            } catch (err) {
+                console.log("Token expired or invalid", err);
+
+                // 👇 yahin token remove karo
+                localStorage.removeItem("authToken");
+
+                // 👇 login page pe bhejo
+                // window.location.href = "/login";
+            }
+        };
+
+        fetchProfile();
+    }, []);
+
+
     return (
+
         <div className="loading-root">
             <div className="loading-content">
                 <img
